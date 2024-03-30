10fb32b9be2768367e15ac58f3fbb91694cadc70
sami <sami@samis-mac-mini.home>
2024-03-30 18:40:16 +0100

    init

---
+++ module/config.py
@@ -0,0 +1,17 @@
+import os
+
+BASE_PATH = "dataset"
+IMAGES_PATH = os.path.sep.join([BASE_PATH, "images"])
+ANNOTS_PATH = os.path.sep.join([BASE_PATH,"annotations"])
+
+BASE_OUTPUT= "output"
+#Output model 
+MODEL_PATH = os.path.sep.join([BASE_OUTPUT, "detector.h5"])
+#Label binarizer
+LB_PATH = os.path.sep.join([BASE_OUTPUT, "lb.pickle"])
+PLOTS_PATH = os.path.sep.join([BASE_OUTPUT, "plots"])
+TEST_PATHS = os.path.sep.join([BASE_OUTPUT, "test_paths.txt"])
+
+INIT_LR = 1e-4
+NUM_EPOCHS = 8
+BATCH_SIZE = 32
